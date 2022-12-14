54cd1cf6fef42b414651abb84f13e1f2598a594c
Jean Paul Forero <jeanpaulforero@gmail.com>
2022-12-14 16:28:24 -0500

    Connecting Currency Exchange Mircoservice with Zipkin

--- currency-exchange-service/src/main/java/com/jpforero/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/jpforero/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,12 @@
 package com.jpforero.microservices.currencyexchangeservice;
 
+import brave.Span;
+import brave.Tracer;
+import brave.sampler.Sampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,15 +15,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
     @Autowired
     private CurrencyExchangeRepository repository;
 
     @Autowired
     private Environment environment;
 
+    @Bean
+    public Sampler alwaysSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from, @PathVariable String to) {
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
